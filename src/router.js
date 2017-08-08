ea321715cc3ae47b5a1fc60ab9782cad098a7945
Jeremy Cho <hyung.cho@hyfn.com>
2017-08-07 20:42:24 -0700

    Adds code-splitting functionality to work with React Router v4

--- src/router.js
+++ src/router.js
@@ -1,7 +1,37 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import App from './components/App'
+
+function asyncComponent(getComponent) {
+  return class AsyncComponent extends React.Component {
+    static Component = null;
+    state = { Component: AsyncComponent.Component }
+
+    componentWillMount() {
+      if (!this.state.Component) {
+        getComponent().then(Component => {
+          AsyncComponent.Component = Component
+          this.setState({ Component })
+        })
+      }
+    }
+
+    render() {
+      const { Component } = this.state
+
+      if (Component) {
+        return <Component {...this.props} />
+      }
+
+      return null
+    }
+  }
+}
+
+// Components
+const App = asyncComponent(() =>
+  System.import('./components/App').then(module => module.default)
+)
 
 const AppRouter = () => (
   <BrowserRouter>
